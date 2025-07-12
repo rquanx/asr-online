87b9af6953861ec29fe600d9cbd71ce83fd4c733
rquanx <runquant@gmail.com>
2025-07-12 10:05:54 +0800

    chore: wasm copy for prod

--- next.config.ts
+++ next.config.ts
@@ -8,16 +8,26 @@ const nextConfig: NextConfig = {
       // 找到 WASM 文件在 node_modules 中的位置
       const wasmPath = require.resolve('./node_modules/sherpa-onnx/sherpa-onnx-wasm-nodejs.wasm');
       const outputPath = path.join(__dirname, '.next/server/vendor-chunks/');
+      const productPath = path.join(__dirname, '.next/server/app/api/sherpa-ncnn/');
 
       // 确保目录存在
       if (!fs.existsSync(outputPath)) {
         fs.mkdirSync(outputPath, { recursive: true });
       }
 
+      if (!fs.existsSync(productPath)) {
+        fs.mkdirSync(productPath, { recursive: true });
+      }
+
       // 复制文件
       fs.copyFileSync(
         wasmPath,
         path.join(outputPath, 'sherpa-onnx-wasm-nodejs.wasm')
+      );
+
+      fs.copyFileSync(
+        wasmPath,
+        path.join(productPath, 'sherpa-onnx-wasm-nodejs.wasm')
       );
     }
     return config;
