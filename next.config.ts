a58280b1209529568dfbfc18135249fbf071615e
rquanx <runquant@gmail.com>
2025-07-12 10:45:46 +0800

    chore: update output file config

--- next.config.ts
+++ next.config.ts
@@ -4,51 +4,54 @@ const fs = require('fs');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    '/api/sherpa-ncnn': ['./sherpa-onnx-wasm-nodejs.wasm']
+  },
   webpack: (config, { isServer }) => {
     // if (isServer) {
-      // 找到 WASM 文件在 node_modules 中的位置
-      const wasmPath = require.resolve('./node_modules/sherpa-onnx/sherpa-onnx-wasm-nodejs.wasm');
-      const outputPath = path.join(__dirname, '.next/server/vendor-chunks/');
-      const productPath = path.join(__dirname, '.next/server/app/api/sherpa-ncnn/');
+    // 找到 WASM 文件在 node_modules 中的位置
+    const wasmPath = require.resolve('./node_modules/sherpa-onnx/sherpa-onnx-wasm-nodejs.wasm');
+    const outputPath = path.join(__dirname, '.next/server/vendor-chunks/');
+    const productPath = path.join(__dirname, '.next/server/app/api/sherpa-ncnn/');
 
-      // 确保目录存在
-      //   if (!fs.existsSync(outputPath)) {
-      //     fs.mkdirSync(outputPath, { recursive: true });
-      //   }
+    // 确保目录存在
+    //   if (!fs.existsSync(outputPath)) {
+    //     fs.mkdirSync(outputPath, { recursive: true });
+    //   }
 
-      //   if (!fs.existsSync(productPath)) {
-      //     fs.mkdirSync(productPath, { recursive: true });
-      //   }
+    //   if (!fs.existsSync(productPath)) {
+    //     fs.mkdirSync(productPath, { recursive: true });
+    //   }
 
-      //   // 复制文件
-      //   fs.copyFileSync(
-      //     wasmPath,
-      //     path.join(outputPath, 'sherpa-onnx-wasm-nodejs.wasm')
-      //   );
+    //   // 复制文件
+    //   fs.copyFileSync(
+    //     wasmPath,
+    //     path.join(outputPath, 'sherpa-onnx-wasm-nodejs.wasm')
+    //   );
 
-      //   fs.copyFileSync(
-      //     wasmPath,
-      //     path.join(productPath, 'sherpa-onnx-wasm-nodejs.wasm')
-      //   );
-      // }
+    //   fs.copyFileSync(
+    //     wasmPath,
+    //     path.join(productPath, 'sherpa-onnx-wasm-nodejs.wasm')
+    //   );
+    // }
 
-      config.plugins.push(
-        new CopyPlugin({
-          patterns: [
-            {
-              from: wasmPath,
-              to: path.join(outputPath, 'sherpa-onnx-wasm-nodejs.wasm'),
-            },
-            {
-              from: wasmPath,
-              to: path.join(productPath, 'sherpa-onnx-wasm-nodejs.wasm'),
-            },
-          ],
-        })
-      );
-      return config;
-    }
-    /* config options here */
-  };
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: wasmPath,
+            to: path.join(outputPath, 'sherpa-onnx-wasm-nodejs.wasm'),
+          },
+          {
+            from: wasmPath,
+            to: path.join(productPath, 'sherpa-onnx-wasm-nodejs.wasm'),
+          },
+        ],
+      })
+    );
+    return config;
+  }
+  /* config options here */
+};
 
-  export default nextConfig;
+export default nextConfig;
