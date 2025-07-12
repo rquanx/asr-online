2415f0c2296b062827df24b99dee5c7670f84f84
rquanx <runquant@gmail.com>
2025-07-12 10:02:28 +0800

    chore: remove eslint check

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,11 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', {
-    rules: {
-      '@typescript-eslint/no-require-imports': 'off',
-    },
-  }),
+  // ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  // {
+  //   rules: {
+  //     '@typescript-eslint/no-require-imports': 'off',
+  //     ' @typescript-eslint/no-unused-vars': 'off',
+  //   },
+  // },
 ]
 
 export default eslintConfig
